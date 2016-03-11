913d5959b97de31e8634565c7d087032091451d9
Geoffroy Couprie <geo.couprie@gmail.com>
2016-03-11 09:27:34 +0100

    parse first tag's header

--- src/parser.rs
+++ src/parser.rs
@@ -36,6 +36,56 @@ named!(pub header<Header>,
   )
 );
 
+#[derive(Debug,PartialEq,Eq)]
+pub enum TagType {
+  Audio,
+  Video,
+  Script,
+}
+
+#[derive(Debug,PartialEq,Eq)]
+pub struct TagHeader {
+  tag_type:  TagType,
+  data_size: u32,
+  timestamp: u32,
+  stream_id: u32,
+}
+
+#[derive(Debug,PartialEq,Eq)]
+pub enum TagData {
+  Audio,
+  Video,
+  Script,
+}
+
+#[derive(Debug,PartialEq,Eq)]
+pub struct Tag {
+  header: TagHeader,
+  data: TagData,
+}
+
+named!(pub tag_header<TagHeader>,
+  chain!(
+    tag_type: switch!(be_u8,
+      8  => value!(TagType::Audio) |
+      9  => value!(TagType::Video) |
+      18 => value!(TagType::Script)
+    )                                ~
+    data_size:          be_u24       ~
+    timestamp:          be_u24       ~
+    timestamp_extended: be_u8        ~
+    stream_id:          be_u24       ,
+    || {
+      TagHeader {
+        tag_type:  tag_type,
+        data_size: data_size,
+        timestamp: (timestamp_extended as u32) << 24 + timestamp,
+        stream_id: stream_id,
+      }
+    }
+  )
+);
+
 #[allow(non_uppercase_globals)]
 #[cfg(test)]
 mod tests {
@@ -65,6 +115,30 @@ mod tests {
       IResult::Done(
         &b""[..],
         Header { version: 1, audio: true, video: true, offset: 9 }
+    ));
+  }
+
+  #[test]
+  fn first_tag_headers() {
+    // starts at 99 bytes (header) + 4 (size of previous tag)
+    // header is 11 bytes long
+    assert_eq!(
+      tag_header(&zelda[13..24]),
+      IResult::Done(
+        &b""[..],
+        TagHeader { tag_type: TagType::Video, data_size: 537, timestamp: 0, stream_id: 0 }
+    ));
+    assert_eq!(
+      tag_header(&zeldaHQ[13..24]),
+      IResult::Done(
+        &b""[..],
+        TagHeader { tag_type: TagType::Video, data_size: 2984, timestamp: 0, stream_id: 0 }
+    ));
+    assert_eq!(
+      tag_header(&commercials[13..24]),
+      IResult::Done(
+        &b""[..],
+        TagHeader { tag_type: TagType::Script, data_size: 273, timestamp: 0, stream_id: 0 }
     ));
   }
 }

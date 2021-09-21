3a0a9883c8f855c7b5733cfb89a2eaf0c70f4865
Constantin Nickel <constantin.nickel@gmail.com>
2021-09-21 11:33:49 +0200

    Update fuzzing code

--- fuzz/fuzzers/tag.rs
+++ fuzz/fuzzers/tag.rs
@@ -1,10 +1,8 @@
 #![no_main]
-extern crate libfuzzer_sys;
-extern crate flavors;
+use libfuzzer_sys::fuzz_target;
 
 use flavors::parser::complete_tag;
 
-#[export_name="rust_fuzzer_test_input"]
-pub extern fn go(data: &[u8]) {
-    let tag_result = complete_tag(data);
-}
+fuzz_target!(|data: &[u8]| {
+    let _tag_result = complete_tag(data);
+});
